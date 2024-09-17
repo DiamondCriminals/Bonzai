4efc06b66286410f0be25f64161291e04ab0390d
antonisoaho <isoahoanton@gmail.com>
2024-09-17 12:37:49 +0200

    Get rooms

--- functions/GetRooms/index.js
+++ functions/GetRooms/index.js
@@ -1,14 +1,23 @@
 const { sendError, sendResponse } = require('../../services/responses');
-const { GetCommand } = require('@aws-sdk/lib-dynamodb');
+const { QueryCommand } = require('@aws-sdk/lib-dynamodb');
 const { db } = require('../../services/db');
-const { v4: uuid } = require('uuid');
 
 const TABLE_NAME = 'bonzai_rooms';
 
 exports.handler = async (event) => {
+  const params = {
+    TableName: TABLE_NAME,
+    KeyConditionExpression: 'id = :id',
+    ExpressionAttributeValues: {
+      ':id': 'ROOM',
+    },
+  };
   try {
-    sendResponse(201, newRoom);
+    const command = new QueryCommand(params);
+    const { Items } = await db.send(command);
+
+    return sendResponse(201, Items);
   } catch (err) {
-    sendError(500, err.message);
+    return sendError(500, err.message);
   }
 };
